e4fdb1b29326304277a5b9114df40c1df10d0537
DaviRibeiro <davirb21@gmail.com>
2020-11-05 20:24:52 -0300

    Preenchendo campos

--- features/cadastro.feature
+++ features/cadastro.feature
@@ -8,9 +8,9 @@ Funcionalidade: Cadastro de Usúarios
 Cenario: Cadastro 
     Dado que acesso a página de cadastro
     Quando submeto o meu cadastro com email:
-    |email           |davirb@gmail.com 
-    |senha           |1234
-    |senha_confirmada|1234
+    |email           | davirb@gmail.com |
+    |senha           | 1234 |
+    |senha_confirmada| 1234 |
     Então devo ser redirecionado para a área logada
 
 Cenario: Email não informado
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,11 +1,15 @@
 Dado('que acesso a página de cadastro') do
         visit "http://parodify.qaninja.com.br/"
-        sleep 5 # temporario
+        click_on "EXPERIMENTE AGORA"
     end
   
   Quando('submeto o meu cadastro com email:') do |table|
-        # table is a Cucumber::MultilineArgument::DataTable
-        pending # Write code here that turns the phrase above into concrete actions
+       user = table.rows_hash
+       find("input[name*=email]").set user[:email]
+       find("input[placeholder='Sua senha secreta']").set user[:senha]
+       find("input[placeholder='Confirme a senha']").set user[:senha_confirmada]
+
+       sleep 5 #temporario
     end
   
   Então('devo ser redirecionado para a área logada') do
